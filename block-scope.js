a966bb6a80c10840ceb20a8ec48fa3def89acd79
Sean Blessing <snblessing@gmail.com>
2020-09-03 14:10:15 -0400

    init commit

---
+++ block-scope.js
@@ -0,0 +1,20 @@
+function testIt() {
+	a = 5;
+	let b = 6;
+	var c = 7;
+}
+testIt();
+console.log(a);
+// 	console.log(b);
+//console.log(c);
+
+if (a==5) {
+	console.log("inside an if");
+	var d = 11;
+	let e = 22;
+	f = 33;
+}
+
+console.log(d);
+//console.log(e);
+console.log(f);
